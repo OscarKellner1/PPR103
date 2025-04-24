b4343511aff7095621e740eb401513b565d27779
MagnMegin <magne.myran@skomakerhuset.no>
2025-04-24 11:35:57 +1000

    Added support for grounded system OnCheck event and added triangleIndex to result

--- Assets/Scripts/PlayerCharacter/GroundedSystem.cs
+++ Assets/Scripts/PlayerCharacter/GroundedSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GroundedSystem : MonoBehaviour
 {
@@ -13,19 +14,24 @@ public class GroundedSystem : MonoBehaviour
 
     public float GroundedSpherecastLength => groundedSpherecastLength;
     public LayerMask GroundedSpherecastMask => groundedSpherecastMask;
+    public UnityEvent<GroundCheckResult> OnCheckGround { get; private set; }
 
     private void Start()
     {
         sphereCastRadius = GetComponent<CapsuleCollider>().radius;
     }
 
+    /// <summary>
+    /// Should only be called by character controller to control the correct executino order.
+    /// </summary>
+    /// <returns></returns>
     public GroundCheckResult CheckGrounded()
     {
         Ray ray = new(transform.position + transform.up * (sphereCastRadius + 0.1f), -transform.up);
         Debug.DrawRay(ray.origin, ray.direction);
         if (Physics.SphereCast(ray, sphereCastRadius, out RaycastHit raycastHit, groundedSpherecastLength + 0.1f, groundedSpherecastMask))
         {
-            return GroundCheckResult.Hit(raycastHit.normal);
+            return GroundCheckResult.Hit(raycastHit.normal, raycastHit.triangleIndex);
         }
         else
         {
@@ -38,20 +44,22 @@ public readonly struct GroundCheckResult
 {
     public readonly bool hit;
     public readonly Vector3 normal;
+    public readonly int triangleIndex;
 
-    private GroundCheckResult(bool hit, Vector3 normal)
+    private GroundCheckResult(bool hit, Vector3 normal, int triangleIndex)
     {
         this.hit = hit;
         this.normal = normal;
+        this.triangleIndex = triangleIndex;
     }
 
-    public static GroundCheckResult Hit(Vector3 normal)
+    public static GroundCheckResult Hit(Vector3 normal, int triangleIndex)
     {
-        return new GroundCheckResult(true, normal);
+        return new GroundCheckResult(true, normal, triangleIndex);
     }
 
     public static GroundCheckResult NoHit()
     {
-        return new GroundCheckResult(false, Vector3.zero);
+        return new GroundCheckResult(false, Vector3.zero, 0);
     }
 }
